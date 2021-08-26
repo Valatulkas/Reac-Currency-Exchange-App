d0afbbddab8e139556c5ac15262a4afa86b5bc52
jmferg11 <jfergie93@gmail.com>
2021-08-26 11:20:43 -0400

    remove old React logos and purged App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,6 @@ import Skeleton from './structure';
 import SingleConversion from './single-conversion';
 import './App.css';
 
-const Home = () => {
-  return <h2>Home</h2>;
-}
-
-const About = () => {
-  return <h2>About</h2>;
-}
-
-const Contact = () => {
-  return <h2>Contact</h2>
-}
-
-const NotFound = () => {
-  return <h2>404 Not Found</h2>
-}
 
 const App = () => {
   return (
