3da3ffb87a8789f1a3c17663d130fba7f0b24829
jmferg11 <jfergie93@gmail.com>
2021-08-26 17:58:34 -0400

    further developing main componentry logic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Skeleton from './structure';
 import SingleConversion from './single-conversion';
+import ConversionComponent from './Conversion-Component';
 import './App.css';
 import MainComponent from './main-component';
 
@@ -12,6 +13,7 @@ const App = () => {
       <Skeleton>
         <Route path='/' exact component={SingleConversion} />
         <Route path='/' component={MainComponent} />
+        <Route path='/' component={ConversionComponent} />
       </Skeleton>
     </Router>
 
--- src/Conversion-Component.js
+++ src/Conversion-Component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Chart from 'chart.js';
 import currencies from './currencies';
 import { checkStatus, json } from './utils';
 
@@ -13,7 +14,7 @@ class ConversionComponent extends React.Component {
             quoteAcronym: params.get('quote') || 'DKK',
             loading: false,
         };
-        this.chartREF = REACT.createRef();
+        this.chartREF = React.createRef();
     }
     componentDidMount () {
         const { baseAcronym, quoteAcronym} = this.state;
@@ -38,4 +39,104 @@ class ConversionComponent extends React.Component {
             })
             .catch(error => console.error(error.message));
     }
+    buildChart = (labels, data, label) => {
+        const chartRef = this.chartRef.curent.getContext('2d');
+        if (typeof this.chart !== 'undefined') {
+            this.chart.destroy();
+        }
+        this.chart = new Chart(this.chartRef.current.getContext("2d"), {
+            type: 'line',
+            data: {
+                labels,
+                datasets: [
+                    {
+                        label: label,
+                        data,
+                        fill: false,
+                        tension: 0,
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+            }
+        })
+    }
+    toBase(amount, rate) {
+        return amount * (1 / rate);
+    }
+    toQuote(amount, rate) {
+        return amount * rate;
+    }
+    convert(amount, rate, equation) {
+        const input = parseFloat(amount);
+        if (Number.isNaN(input)) {
+            return '';
+        }
+        return equation(input, rate).toFixed(2);
+    }
+    changeBaseAcronym = (event) => {
+        const baseAcronym = event.target.value;
+        this.setState({ baseAcronym });
+        this.getRate(baseAcronym, this.state.quoteAcronym);
+    }
+    changeBaseValue = (event) => {
+        const quoteValue = this.convert(event.target.value, this.state.rate, this.toQuote);
+        this.setState({
+            baseValue: event.target.value,
+            quoteValue,
+        });
+    }
+    changeQuoteAcronym = (event) => {
+        const quoteAcronym = event.target.value;
+        this.setState({ quoteAcronym });
+        this.getRate(this.state.baseAcronym, quoteAcronym);
+    }
+    changeQuoteValue = (event) => {
+        const baseValue = this.convert(event.target.value, this.state.rate, this.toBase);
+        this.setState({
+            quoteValue: event.target.value,
+            baseValue,
+        });
+    }
+    render () {
+        const { rate, baseAcronym, baseValue, quoteAcronym, quoteValue, loading } = this.state;
+        const currencyList = Object.keys(currencies).map(currencyAcronym => <option key={currencyAcronym} value={currencyAcronym}>{currencyAcronym}</option>);
+        return (
+            <React.Fragment>
+                <div>
+                    <h3>Convert Your Currencies</h3>
+                    <h4>1 {baseAcronym} to 1 {quoteAcronym} = {rate.toFixed(4)} {currencies[quoteAcronym].name}</h4>
+                </div>
+                <form className='bg-light justify-content-center'>
+                    <div className='col-md-6 form-group'>
+                        <select value={baseAcronym} onChange={this.changeBaseAcronym} className='form-control' disabled={loading}>
+                            {currencyList}
+                        </select>
+                        <div className='inputs'>
+                            <div className='inputs-text'>{currencies[baseAcronym].symbol}</div>
+                        </div>
+                        <input id='base-currency' className='form-control' value={baseValue} onChange={this.changeBaseValue} type='number' />
+                        <small>(currencies[baseAcronym].name</small>
+                   </div>
+                   <div className='col-md-2 d-flex justify-content-center align-items-center'>
+                       <h4>=</h4>
+                   </div>
+                   <div className='col-md-6 form-group'>
+                       <select value={quoteAcronym} onChange={this.changeQuoteAcronym} className='form-control' disabled={loading}>
+                           {currencyList}
+                       </select>
+                       <div className='inputs'>
+                            <div className='inputs-text'>{currencies[quoteAcronym].symbol}</div>
+                        </div>
+                        <input id='quote-currency' className='form-control' value={quoteValue} onChange={this.changeQuoteValue} type='number' />
+                        <small>(currencies[quoteAcronym].name</small>
+                   </div>
+                </form>
+                <canvas ref={this.chartRef} />
+            </React.Fragment>
+        )
+    }
 }
+
+export default ConversionComponent
